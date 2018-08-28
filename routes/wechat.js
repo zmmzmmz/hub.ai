ce469b2e5bd4d211f1ba29b0acb8dd80834cf469
zhangming <petterstrong@163.com>
2018-08-28 18:05:12 +0800

    添加自动回复

--- routes/wechat.js
+++ routes/wechat.js
@@ -3,11 +3,14 @@ const crypto = require('crypto');
 const logger = require('../utils/logger');
 const xml2js = require('xml2js');
 const msg = require('../modules/messages');
+const ai = require('../modules/ai');
 const axios = require('axios');
 const router = express.Router();
 const token = 'weixin';
 
+
 router.get('/', async (req, res) => {
+  // 校验签名
   const {signature, timestamp, nonce, echostr} = req.query;
   const query = [token, timestamp, nonce];
   query.sort();
@@ -28,9 +31,18 @@ router.get('/', async (req, res) => {
   })
   req.on('end', () => {
     const msgXml = Buffer.concat(buffer).toString('utf-8');
-    xml2js.parseString(msgXml, {explicitArray : false}, (err, result) => {
+    xml2js.parseString(msgXml, {explicitArray : false}, async (err, result) => {
       if (err) res.status(500).send(err.message)
       result = result.xml
+      console.log(result)
+      if (result.MsgType === 'text') {
+        try {
+          const answer = await ai.fetchAnswer(result.Content)
+          console.log(answer)
+        } catch (e) {
+          console.log(e)
+        }
+      }
       const message = msg.txtMsg(result.FromUserName, result.ToUserName, '你好，作者')
       res.send(message)
     })
